c32583a5d0a7d88d8307d1852f4f0ca91c6186c0
Paramjeet Dhiman <paramjeet@montaigne.co>
2020-02-04 06:58:16 +0530

    DEL: .yarn

--- src/components/card/card-component.js
+++ src/components/card/card-component.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import './card-component.css';
 const Card = (props) => {
   return (
-    <div>
-      <h1>{props.monster.name}</h1>
+    <div className='card-container'>
+      <img
+        alt='monster'
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+      />
+      <h2>{props.monster.name}</h2>
+      <p>{props.monster.email}</p>
     </div>
   );
 };
