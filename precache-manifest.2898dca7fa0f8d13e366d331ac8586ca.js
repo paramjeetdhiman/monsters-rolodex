9964005d18c0b53f73edfc8ac99dfce88647d533
Paramjeet Dhiman <paramjeet@montaigne.co>
2020-02-04 14:38:10 +0530

    Updates

---
+++ precache-manifest.2898dca7fa0f8d13e366d331ac8586ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec86e9d9f66381d1b41751e475c5613",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2e1ec4c7ca4708a4f6fa",
+    "url": "/monsters-rolodex/static/css/main.661af64d.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e1ec4c7ca4708a4f6fa",
+    "url": "/monsters-rolodex/static/js/main.bb2d7216.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
